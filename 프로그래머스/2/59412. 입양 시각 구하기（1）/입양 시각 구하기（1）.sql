6afa825fb5ad945c11ae0222b54b3fa1a2cb95be
beginninggrace <151606621+beginninggrace@users.noreply.github.com>
2024-01-05 09:24:21 +0900

    Update 입양 시각 구하기（1）.sql

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -6,3 +6,11 @@ from ANIMAL_OUTS
 where Extract(Hour from DATETIME) >=9 and Extract(Hour from DATETIME) < 20
 group by Extract(Hour from DATETIME)
 order by 1 
+
+-- 혹은 다른 답안
+SELECT substring(datetime,12,2) as hour,
+count(*)
+from animal_outs
+where substring(datetime,12,2)  between 9 and 19
+group by hour
+order by hour
