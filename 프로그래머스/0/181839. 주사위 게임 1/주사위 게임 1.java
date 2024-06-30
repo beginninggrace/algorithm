6929392ff048960be2a4eedf58a1f83edbd5650d
sieun kim <151606621+beginninggrace@users.noreply.github.com>
2024-07-01 01:46:55 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        if(a%2==0 && b%2==0)
-        {
+        if(a%2==0 && b%2==0) {
             answer = Math.abs(a-b);
         }else if(a%2!=0 && b%2!=0) {
             answer = a*a + b*b;
